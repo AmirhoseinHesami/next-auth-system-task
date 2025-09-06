f4cb8281da87cb4cc7687cf12039718f3115f1fa
Amirhosein Hesami <amirhossein.hesami08@gmail.com>
2025-09-06 22:00:17 +0330

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "randomuser.me",
+        hostname: "randomuser.me/api",
       },
     ],
   },
